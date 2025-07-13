231f5f9921e8d8d99decbd9323ae46f72c2c1586
vidhigoyal-startappss <vidhi.goyal@startappss.com>
2025-07-13 22:37:01 +0530

    updated main.ts file

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const allowedOrigin = configService.get<string>('CORS_ORIGIN', 'http://localhost:5173');
-  const port = configService.get<number>('PORT') || process.env.PORT || 3000;
+  const port = process.env.PORT || configService.get<number>('PORT') || 3000;
 
 
   app.enableCors({
