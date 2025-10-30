79b936e511497539cba1ce278383bd91baecc4ed
Ishan-0012 <ishan.shrivastava@startappss.com>
2025-10-30 11:49:47 +0530

    Fix: Latest backend23  commit for deployment

--- src/letter/letter.controller.ts
+++ src/letter/letter.controller.ts
@@ -183,11 +183,11 @@ export class LetterController {
       });
 
       // const browser = await puppeteer.launch({ headless: true });
-     
-const browser = await puppeteer.launch({
-  executablePath: executablePath(), // uses Puppeteer's downloaded Chromium
+     const browser = await puppeteer.launch({
+  headless: true,
   args: ['--no-sandbox', '--disable-setuid-sandbox'],
 });
+
       const page = await browser.newPage();
       await page.setUserAgent(
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36"
